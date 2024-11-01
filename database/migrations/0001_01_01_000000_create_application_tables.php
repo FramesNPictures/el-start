1eefcd86e3ddfa77402ccca02a4f69cb564d5d84
Chris Cynarski <chris@cynarski.net>
2024-11-01 20:16:48 +0000

    Refactor AppSetting to use 'uuid' instead of 'key'
    Updated methods in AppSetting to utilize 'uuid' rather than 'key' for consistency and uniqueness. Removed 'key' field from 'app_settings' table in the migration file to reflect this change.

--- database/migrations/0001_01_01_000000_create_application_tables.php
+++ database/migrations/0001_01_01_000000_create_application_tables.php
@@ -14,7 +14,6 @@ return new class extends Migration
         // Application Settings
         Schema::create('app_settings', function(Blueprint $table) {
             $table->char('uuid', 16)->charset('binary')->primary();
-            $table->char('key', 32)->charset('binary')->index();
             $table->text('value');
             $table->timestamps();
         });
--- src/Helpers/AppSetting.php
+++ src/Helpers/AppSetting.php
@@ -10,8 +10,8 @@ class AppSetting
 {
     public static function getRaw(string $key, $default = null): mixed
     {
-        $key = md5(strtolower($key));
-        $rec = AppSettingModel::where('key', $key)->first();
+        $uuid = md5(strtolower($key));
+        $rec = AppSettingModel::where('uuid', $uuid)->first();
 
         if (!$rec) {
             return $default;
@@ -22,29 +22,28 @@ class AppSetting
 
     public static function get(string $key, $default = null): mixed
     {
-        $key = md5(strtolower($key));
-        return Cache::remember(__CLASS__ . $key, 60 * 60, function () use ($default, $key) {
+        $uuid = md5(strtolower($key));
+        return Cache::remember(__CLASS__ . $uuid, 60 * 60, function () use ($default, $key) {
             return self::getRaw($key, $default);
         });
     }
 
     public static function set(string $key, $value)
     {
-        $uuid = Str::uuid();
-        $key = md5(strtolower($key));
+        $uuid = md5(strtolower($key));
         $value = base64_encode(encrypt($value));
         $rec = AppSettingModel::updateOrCreate(
-            ['key' => $key],
-            ['key' => $key, 'value' => $value, 'uuid' => $uuid]
+            ['uuid' => $uuid],
+            ['uuid' => $uuid, 'value' => $value]
         );
 
-        Cache::set(__CLASS__ . $key, $value);
+        Cache::set(__CLASS__ . $uuid, $value);
     }
 
     public static function forget(string $key)
     {
-        $key = md5(strtolower($key));
-        AppSettingModel::where('key', $key)->delete();
-        Cache::forget($key);
+        $uuid = md5(strtolower($key));
+        AppSettingModel::where('uuid', $key)->delete();
+        Cache::forget(__CLASS__ . $uuid);
     }
 }
