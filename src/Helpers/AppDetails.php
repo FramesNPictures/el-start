d125c936a7c960df3c0e68a3cc9fde8c86d56877
Chris Cynarski <chris@cynarski.net>
2024-11-02 16:24:09 +0000

    Switch to updateOrCreate method for setting user details
    This change replaces the createOrUpdate method with updateOrCreate to ensure that existing user details are properly updated or created as needed. This enhances data consistency and aligns with the intended functionality of setting user details in the application.

--- src/Helpers/AppDetails.php
+++ src/Helpers/AppDetails.php
@@ -10,7 +10,7 @@ class AppDetails
 {
     public static function set(AppUserModel $user, UserDetailType $type, $value, $tab = 0)
     {
-        AppUserDetailModel::createOrUpdate(
+        AppUserDetailModel::updateOrCreate(
             ['user_id' => $user->id, 'type' => $type, 'tab' => $tab],
             ['user_id' => $user->id, 'type' => $type, 'tab' => $tab, 'value' => $value],
         );
